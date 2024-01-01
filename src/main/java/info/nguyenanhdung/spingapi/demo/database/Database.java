45a2939f19b5a0b6e278e19017a3e00d358d2df9
dungna <dungna@outlook.com>
2024-01-01 10:14:06 +0700

    firt commit

---
+++ src/main/java/info/nguyenanhdung/spingapi/demo/database/Database.java
@@ -0,0 +1,23 @@
+package info.nguyenanhdung.spingapi.demo.database;
+
+import info.nguyenanhdung.spingapi.demo.models.Product;
+import info.nguyenanhdung.spingapi.demo.repositories.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Database {
+    // logger
+    private static final Logger logger = LoggerFactory.getLogger(Database.class);
+    @Bean
+    CommandLineRunner intDatabase(ProductRepository productRepository) {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+            }
+        };
+    }
+}
